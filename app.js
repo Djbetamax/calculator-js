0cb6f74ca552f8c7cb37d3c9c14a256f4dcd3aa8
djbetamax <hugots23@gmail.com>
2022-08-10 16:59:30 -0300

    added: nothing on this file. In progress

---
+++ app.js
@@ -0,0 +1,24 @@
+// Pega o número precionado e mostra no display
+const calculator = document.getElementById('calculator');
+const keys = document.getAnimations('keys')
+const display = document.querySelector('display')
+
+
+// Operadores
+
+  // Soma
+
+
+  // Subtração
+
+
+  // Multiplicação
+
+
+  // Divisão
+  
+
+//? Pega os números junto com o operador e joga no display
+
+//? Resultado
+
